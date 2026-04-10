5011b50891ee7551acb6f3e5b1bedaa4431f87f2
School_Projct <shaileshmrj2001@gmail.com>
2026-04-10 10:32:39 +0530

    second committ

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,39 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   const navigate = useNavigate();
   const courses = [
-    { title: "B.Sc Computer Science", duration: "3 years", seats: 60 },
-    { title: "B.Com", duration: "3 years", seats: 80 },
-    { title: "M.Sc Physics", duration: "2 years", seats: 30 },
+    {
+      title: "Humanities",
+      subject:
+        "1.Hindi 2.English 3.Sanskrit 4.Geography 5.Civics 6.Economics 7.Music Vocal",
+      seats: 60,
+    },
+    {
+      title: "Science",
+      subject:
+        "1.General Hindi 2.English 3.physics 4.Chemistry 5.Biology 6.Math 7.Computer ",
+      seats: 50,
+    },
+    {
+      title: "Commerce",
+      subject:
+        "1.General Hindi 2.English 3.Accountancy 4.Business Studies 5.Economics",
+      seats: 80,
+    },
+    {
+      title: "Vocational",
+      subject:
+        "1.General Hindi 2.English 3.Typing Hindi & English 4.General Foundational",
+      seats: 30,
+    },
+    {
+      title: "Agriculture",
+      subject:
+        "1.Agronomy sixith 2.Ag. Economics 3.Ag Zoology 4.Animal Husbandry 5.Ag.Chemistry",
+      seats: 30,
+    },
+    { title: "NCC", subject: "NCC", seats: 30 },
+    { title: "Scout & Guide", subject: "Scout & Guide", seats: 30 },
+    { title: "Music vocal", subject: "Music vocal", seats: 30 },
   ];
 
   return (
